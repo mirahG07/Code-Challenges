761d062a5fb756978e0600eb477cfa2ce57fecbf
Minh498855 <498855@eriesd.org>
2024-02-12 15:07:42 -0500

    made checknum

---
+++ Check Nums/problem.py
@@ -0,0 +1,25 @@
+'''
+Have the function CheckNums(num1,num2) take both parameters being passed and 
+return the string true if num2 is greater than num1, otherwise return the 
+string false. If the parameter values are equal to each other then return the string -1.
+
+Input: 3 & num2 = 122
+Output: true
+
+Input: 67 & num2 = 67
+Output: -1
+'''
+def check_nums (num1, num2):
+    '''
+    This function check 2 parameters. It will return true if num2 is greater than num1, 
+    otherwise return the string false. If the parameter values are equal to each other 
+    then return the string -1.
+
+    Input: 3 & num2 = 122
+    Output: true
+
+    Input: 67 & num2 = 67
+    Output: -1
+    '''
+    pass
+
